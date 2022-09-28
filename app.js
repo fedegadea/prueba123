14e888e32fd4dc7ca47894b0987a27f16cef5903
fedegadea <federicosegundogadea@gmail.com>
2022-09-28 18:33:01 -0300

    s

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const path= require("path");
 const publicPath= path.resolve(__dirname, "/public");
 app.use(express.static(publicPath));
-
 app.listen(process.env.PORT || 3000,() => { 
     
     console.log("Servidor corriendo")
